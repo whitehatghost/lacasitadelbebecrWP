477a3cd03ff4ec35d89545d1af3df7be9767ab5d
whitehatghost <113263264+whitehatghost@users.noreply.github.com>
2025-06-12 01:09:16 -0600

    Update header.php

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
       justify-content: space-between;
       align-items: center;
       flex-wrap: wrap;
-      box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+      border-bottom: 0;
     }
 
     @keyframes flowGradient {
